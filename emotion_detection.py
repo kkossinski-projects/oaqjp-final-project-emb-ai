e2b5a13eaad3bb537c90dfc19cb82b312d98eebd
kkossinski-projects <kkossinski@gmail.com>
2025-08-22 16:58:11 +0100

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,26 @@ def emotion_detector(text_to_analyse):  # Define a function
     header= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API
-    return response.text  # Return the response text from the API
+    # Parsing the JSON response from the API
+   
+    formatted_response = json.loads(response.text)
+   
+    # Extracting emotions from the response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    # Establishing which is the strongest emotion
+    
+    # Returning a dictionary containing sentiment analysis results
+
+    return {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': '<name of the dominant emotion>'
+    }
